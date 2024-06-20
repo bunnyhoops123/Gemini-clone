ab6d7c25aaee897b60a28baa584aabf1427e7598
sumukhM80 <sumukh@mobil80.com>
2024-06-20 11:25:50 +0530

    UI done

--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -1,43 +1,58 @@
-import React from 'react'
-import './Main.css';
-import { assets } from '../../assets/assets';
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
 
 const Main = () => {
   return (
-    <div className='main'>
-        <div className="nav">
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt="" />
-        </div>
-    <div className="main-container">
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
         <div className="greet">
-            <p><span>Hello, Sumukh</span></p>
-            <p>How can I help you today?</p>
+          <p>
+            <span>Hello, Sumukh</span>
+          </p>
+          <p>How can I help you today?</p>
         </div>
         <div className="cards">
-            <div className="card">
-                <p>Suggest beautiful places to see in Bangalore</p>
-                <img src={assets.compass_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Briefly summarize this concept: Urban planning</p>
-                <img src={assets.bulb_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Brainstorm team bonding activites for our work retreat</p>
-                <img src={assets.message_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Improve the readability of the following code</p>
-                <img src={assets.code_icon} alt="" />
-            </div>
+          <div className="card">
+            <p>Suggest beautiful places to see in Bangalore</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: Urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activites for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
         </div>
-        <div className='main-bottom'>
-            
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter prompt here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <div className="bottom-info">
+            <p>
+              Gemini may display inaccurate info, including about people, so
+              double-check its responses. *This is a demo website.*
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
